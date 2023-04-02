6b42b12721a02fb2c9145524cf62d235946546ee
Darshan <darshan.nagesh10@gmail.com>
2023-04-02 22:05:37 +0530

    confirmPassword added

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,38 +2,68 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const SignUp = (props) => {
+  const [cred, setCred] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+  const [error, setError] = useState("");
 
-
-  const [cred, setCred] = useState({username:"",  email: "", password: "",cpassword :""  })
-
-  const onChange = (e) => {
-    setCred({ ...cred, [e.target.name]: e.target.value })
-  }
-
-  let history = useNavigate();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    if (cred.password !== cred.confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
     const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username: cred.username, email: cred.email, password: cred.password }),
+      body: JSON.stringify({
+        username: cred.username,
+        email: cred.email,
+        password: cred.password
+      })
     });
-    const json = await response.json();
-    console.log(json)
-    if (json.success) {
-      // Save auth token and redirect 
-      localStorage.setItem('token', json.authToken)
-      history("/")
-      props.showAlert('Account Created Successfully ', 'success')
-    }
-    else {
-      props.showAlert('Invalid Credentials ', 'warning')
-    }
-  }
 
+    const json = await response.json();
+    if (json.success) {
+      localStorage.setItem('token', json.authToken);
+      navigate("/");
+      props.showAlert('Account Created Successfully ', 'success');
+    } else {
+      props.showAlert('Invalid Credentials ', 'warning');
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setCred({ ...cred, [e.target.name]: e.target.value });
+  };
+
+  const handlePasswordChange = (e) => {
+    const password = e.target.value;
+    setCred((prevState) => ({ ...prevState, password }));
+    if (cred.confirmPassword !== password) {
+      setError("Passwords do not match");
+    } else {
+      setError("");
+    }
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    const confirmPassword = e.target.value;
+    setCred((prevState) => ({ ...prevState, confirmPassword }));
+    if (cred.password !== confirmPassword) {
+      setError("Passwords do not match");
+    } else {
+      setError("");
+    }
+  };
 
   return (
     <div className='container'>
@@ -41,20 +71,21 @@ const SignUp = (props) => {
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <div className="mb-3">
-            <label htmlFor="username" className="form-label"><strong> User Name</strong></label>
-            <input type="text" className="form-control" id="username" name="username" onChange={onChange} />
+            <label htmlFor="username" className="form-label"><strong>User Name</strong></label>
+            <input type="text" className="form-control" id="username" name="username" onChange={handleInputChange} />
           </div>
-          <label htmlFor="email" className="form-label"><strong> Email address</strong></label>
-          <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" onChange={onChange} />
+          <label htmlFor="email" className="form-label"><strong>Email address</strong></label>
+          <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" onChange={handleInputChange} />
           <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div className="mb-3">
-          <label htmlFor="password" className="form-label"><strong> Password</strong></label>
-          <input type="password" className="form-control" id="password" name="password" onChange={onChange} minLength={5} required />
+          <label htmlFor="password" className="form-label"><strong>Password</strong></label>
+          <input type="password" className="form-control" id="password" name="password" onChange={handlePasswordChange} minLength={5} required />
         </div>
         <div className="mb-3">
-          <label htmlFor="cpassword" className="form-label"><strong> Confirm Password</strong></label>
-          <input type="password" className="form-control" id="cpassword" name="cpassword" onChange={onChange} minLength={5} required/>
+          <label htmlFor="confirmPassword" className="form-label"><strong>Confirm Password</strong></label>
+          <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" onChange={handleConfirmPasswordChange} minLength={5} required />
+          {error && <div className="text-danger">{error}</div>}
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
